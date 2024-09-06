deb97f5091d69f93ed9d6102e910c08760ec3425
Gabriel Costa Soares <gsoares0404@gmail.com>
2024-09-06 17:12:33 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var perspectiveText = document.querySelector(".perspective-text");
+
+window.addEventListener('DOMContentLoaded', function() {
+  setTimeout(() => {
+    perspectiveText.classList.add("active");
+  }, 400);
+});
